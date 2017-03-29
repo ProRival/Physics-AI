f242db1aedbbf23a267131baad40d28c6682545f
Kyle <igrandslam1@yahoo.com>
2017-03-29 10:48:35 -0500

    Created children of MazePlan. This will provide the capability to have a wide variety of mazes in the future.

---
+++ src/main/java/com/kyleposluns/ai/maze/MazePlan2d.java
@@ -0,0 +1,10 @@
+package com.kyleposluns.ai.maze;
+
+/**
+ * Created by Kyle on 3/28/17.
+ */
+public interface MazePlan2d extends MazePlan {
+
+	MazeCell getCell(int x, int y);
+
+}
---
+++ src/main/java/com/kyleposluns/ai/maze/MazePlan3d.java
@@ -0,0 +1,7 @@
+package com.kyleposluns.ai.maze;
+
+public interface MazePlan3d extends MazePlan {
+
+	MazeCell getCell(int x, int y, int z);
+
+}
