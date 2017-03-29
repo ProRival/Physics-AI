c11541e54ce1cac64eea788066ac88c721e51a8e
Kyle <igrandslam1@yahoo.com>
2017-03-29 10:54:33 -0500

    Created a Maze interface, with typing capabilities, that represents a maze that is ready to be solved.

---
+++ src/main/java/com/kyleposluns/ai/maze/Maze.java
@@ -0,0 +1,17 @@
+package com.kyleposluns.ai.maze;
+
+/**
+ * Created by Kyle on 3/29/17.
+ */
+public interface Maze {
+
+	default void init() {
+
+	}
+
+	default void deinit() {
+
+	}
+
+	MazeType getType();
+}
---
+++ src/main/java/com/kyleposluns/ai/maze/MazeType.java
@@ -0,0 +1,10 @@
+package com.kyleposluns.ai.maze;
+
+/**
+ * Created by Kyle on 3/29/17.
+ */
+public interface MazeType {
+
+	Class<? extends MazePlan> getPlanClass();
+
+}
