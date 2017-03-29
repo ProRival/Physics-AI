5cae42038a378dedfad724c32a7fcd2e01fc987a
Kyle <igrandslam1@yahoo.com>
2017-03-29 10:57:00 -0500

    Started a RectangularMaze implementation.

---
+++ src/main/java/com/kyleposluns/ai/maze/bidimensional/RectangularMaze.java
@@ -0,0 +1,44 @@
+package com.kyleposluns.ai.maze.bidimensional;
+
+import com.kyleposluns.ai.maze.Maze;
+import com.kyleposluns.ai.maze.MazeCell;
+import com.kyleposluns.ai.maze.MazePlan2d;
+import com.kyleposluns.ai.maze.MazeType;
+import com.kyleposluns.ai.maze.MazeType2d;
+import java.awt.Graphics;
+
+public class RectangularMaze implements Maze, MazePlan2d {
+
+	private boolean isGenerated;
+
+	private int width, height;
+
+	public RectangularMaze(int width, int height) {
+		this.width = width;
+		this.height = height;
+		this.isGenerated = false;
+	}
+
+	@Override
+	public void generate() {
+		if (isGenerated) return;
+	}
+
+	@Override
+	public void paint(Graphics graphics) {
+		if (!isGenerated) {
+			generate();
+		}
+
+	}
+
+	@Override
+	public MazeCell getCell(int x, int y) {
+		return null;
+	}
+
+	@Override
+	public MazeType getType() {
+		return MazeType2d.RECTANGULAR;
+	}
+}
