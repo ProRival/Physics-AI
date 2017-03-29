b47b0c824c56d526bcd299b31eeae96c88d185d0
Kyle <igrandslam1@yahoo.com>
2017-03-29 10:47:06 -0500

    Created the interface MazePlan that represents an abstract Maze.

---
+++ src/main/java/com/kyleposluns/ai/maze/MazePlan.java
@@ -0,0 +1,14 @@
+package com.kyleposluns.ai.maze;
+
+import java.awt.Graphics;
+
+/**
+ * Created by Kyle on 3/28/17.
+ */
+public interface MazePlan {
+
+	void generate();
+
+	void paint(Graphics graphics);
+
+}
