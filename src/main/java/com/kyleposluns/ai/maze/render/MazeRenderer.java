8cabc76d4a28d2a98e6aeec8f87077ec9af31f56
Kyle <igrandslam1@yahoo.com>
2017-03-29 22:52:54 -0500

    Turned MazeRenderer into an abstract class to ensure the proper use of the model.

--- src/main/java/com/kyleposluns/ai/maze/render/MazeRenderer.java
+++ src/main/java/com/kyleposluns/ai/maze/render/MazeRenderer.java
@@ -3,8 +3,14 @@ package com.kyleposluns.ai.maze.render;
 import com.kyleposluns.ai.maze.MazeModel;
 import java.awt.Graphics;
 
-public interface MazeRenderer<T extends MazeModel> {
+public abstract class MazeRenderer<T extends MazeModel> {
 
-	void paint(Graphics g);
+	protected T model;
+
+	public MazeRenderer(T model) {
+		this.model = model;
+	}
+
+	public abstract  void paint(Graphics g);
 
 }
