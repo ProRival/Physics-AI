36c595873ccc5636713f4cdec5f6f879ba8c734a
Kyle <igrandslam1@yahoo.com>
2017-03-29 22:29:47 -0500

    Created a CartesianCell to represent a cell in a 2d Cartesian space.

---
+++ src/main/java/com/kyleposluns/ai/maze/bidimensional/impl/CartesianCell.java
@@ -0,0 +1,33 @@
+package com.kyleposluns.ai.maze.bidimensional.impl;
+
+import com.kyleposluns.ai.maze.MazeCell;
+import com.kyleposluns.ai.util.CardinalDirection;
+import com.kyleposluns.ai.util.Location;
+
+public class CartesianCell implements MazeCell {
+
+	private boolean[] walls;
+
+	private Location location;
+
+	public CartesianCell(Location location) {
+		this.location = location;
+		this.walls = new boolean[4];
+	}
+
+	//linear search shouldn't take too long
+	private int getDir(CardinalDirection dir) {
+		for (int i = 0; i < CardinalDirection.values().length; i++) {
+			if (CardinalDirection.values()[i] == dir) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public boolean hasWall(CardinalDirection direction) {
+		return walls[getDir(direction)];
+	}
+
+
+}
