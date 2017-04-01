eb0282d627732f89cb668f9356789b16a2ba57eb
Kyle <igrandslam1@yahoo.com>
2017-04-01 11:52:43 -0500

    Implemented the new paradigm in the RectangularMaze class.

--- src/main/java/com/kyleposluns/ai/maze/bidimensional/impl/rectangular/RectangularMaze.java
+++ src/main/java/com/kyleposluns/ai/maze/bidimensional/impl/rectangular/RectangularMaze.java
@@ -1,4 +1,34 @@
 package com.kyleposluns.ai.maze.bidimensional.impl.rectangular;
 
-public class RectangularMaze {
+import com.kyleposluns.ai.maze.Maze;
+import com.kyleposluns.ai.maze.MazeModel;
+import com.kyleposluns.ai.maze.MazeType;
+import com.kyleposluns.ai.maze.bidimensional.MazeType2D;
+import com.kyleposluns.ai.maze.render.MazeRenderer;
+
+public class RectangularMaze implements Maze{
+
+	private final RectangularMazeModel model;
+
+	private final RectangularMazeRenderer renderer;
+
+	public RectangularMaze(RectangularMazeModel model, RectangularMazeRenderer renderer) {
+		this.model = model;
+		this.renderer = renderer;
+	}
+	
+	@Override
+	public MazeRenderer getRenderer() {
+		return renderer;
+	}
+
+	@Override
+	public MazeModel getModel() {
+		return model;
+	}
+
+	@Override
+	public MazeType getType() {
+		return MazeType2D.RECTANGULAR;
+	}
 }
