79a0a49532f3ce8898de80f270bcc3f36a903d89
Kyle <igrandslam1@yahoo.com>
2017-03-31 19:21:30 -0500

    Reorganized the renderer class.

--- src/main/java/com/kyleposluns/ai/maze/bidimensional/impl/RectangularMazeRenderer.java
+++ src/main/java/com/kyleposluns/ai/maze/bidimensional/impl/RectangularMazeRenderer.java
@@ -5,6 +5,7 @@ import com.kyleposluns.ai.util.CardinalDirection;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import javax.swing.BorderFactory;
 
 public class RectangularMazeRenderer extends MazeRenderer<RectangularMaze> {
 
@@ -13,7 +14,7 @@ public class RectangularMazeRenderer extends MazeRenderer<RectangularMaze> {
 	public RectangularMazeRenderer(RectangularMaze model, int width, int height) {
 		super(model, width, height);
 		init();
-		//this.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK));
+		this.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK));
 	}
 
 	private final void init() {
@@ -33,23 +34,22 @@ public class RectangularMazeRenderer extends MazeRenderer<RectangularMaze> {
 			for (int y = 0; y < model.getColumns(); y++) {
 
 				g.setColor(Color.BLACK);
-
-				if (model.getCell(x, y).hasWall(CardinalDirection.NORTH)) {
-					g.drawLine(x * CELL_WIDTH, (y + 1) * CELL_HEIGHT, (x + 1) * CELL_WIDTH, (y + 1) * CELL_HEIGHT);
-				}
-
-				if (model.getCell(x, y).hasWall(CardinalDirection.WEST)) {
-					g.drawLine(x * CELL_WIDTH, y * CELL_HEIGHT, x * CELL_WIDTH, (y + 1) * CELL_HEIGHT);
-				}
+				drawCell(g, x, y);
 			}
+		}
+
+
+	}
+
+	private void drawCell(Graphics2D g, int x, int y) {
+		if (model.getCell(x, y).hasWall(CardinalDirection.NORTH)) {
+			g.drawLine(x * CELL_WIDTH, (y + 1) * CELL_HEIGHT, (x + 1) * CELL_WIDTH, (y + 1) * CELL_HEIGHT);
+		}
+
+		if (model.getCell(x, y).hasWall(CardinalDirection.WEST)) {
+			g.drawLine(x * CELL_WIDTH, y * CELL_HEIGHT, x * CELL_WIDTH, (y + 1) * CELL_HEIGHT);
 		}
 
 	}
 
-	private void drawCell(int x, int y) {
-		CartesianCell cell = model.getCell(x, y);
-
-		System.out.println("North: " + cell.hasWall(CardinalDirection.NORTH));
-		System.out.println("West: " + cell.hasWall(CardinalDirection.WEST));
-	}
 }
