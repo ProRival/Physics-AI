0ba4971f3b818e404b038f1cd2c17fdbc5caf71d
Kyle <igrandslam1@yahoo.com>
2017-03-31 19:22:36 -0500

    Added a simple driver class for testing.

--- src/main/java/com/kyleposluns/ai/PhysicsAI.java
+++ src/main/java/com/kyleposluns/ai/PhysicsAI.java
@@ -1,11 +1,32 @@
 package com.kyleposluns.ai;
 
 
+import com.kyleposluns.ai.maze.bidimensional.impl.RectangularMaze;
+import com.kyleposluns.ai.maze.bidimensional.impl.RectangularMazeRenderer;
+import com.kyleposluns.ai.maze.render.MazeRenderer;
+
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+
 public class PhysicsAI {
 
 	public static void main(String[] args) {
 
+		JFrame f = new JFrame();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		RectangularMaze maze = new RectangularMaze(25, 25);
+		maze.generate();
+		MazeRenderer<RectangularMaze> renderer = new RectangularMazeRenderer(maze, 500, 500);
+
+		f.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER));
+
+		f.getContentPane().add(renderer);
+		f.pack();
+		f.setVisible(true);
+
 	}
+
 
 
 }
