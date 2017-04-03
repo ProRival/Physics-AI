51209535fc1c400e3805b9e72436501ae5b39e73
Kyle <igrandslam1@yahoo.com>
2017-04-02 22:23:22 -0500

    Created a MazeRenderer interface, its one job is to render a maze.

--- src/main/java/com/kyleposluns/ai/maze/renderer/MazeRenderer.java
+++ src/main/java/com/kyleposluns/ai/maze/renderer/MazeRenderer.java
@@ -1,8 +1,13 @@
 package com.kyleposluns.ai.maze.renderer;
 
+import com.kyleposluns.ai.maze.Maze;
+import java.awt.Graphics;
+
 /**
  * Created by Kyle on 4/2/17.
  */
-public abstract class MazeRenderer {
+public interface MazeRenderer<T extends Maze> {
+
+	void render(T maze, Graphics g);
 
 }
