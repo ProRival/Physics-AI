584df6cf7c9ce2b79e7a3799e11fe0f564a2ef08
Kyle <igrandslam1@yahoo.com>
2017-04-22 17:12:17 -0500

    Reorganized solver logic.

--- src/main/java/com/kyleposluns/ai/maze/Maze.java
+++ src/main/java/com/kyleposluns/ai/maze/Maze.java
@@ -3,12 +3,14 @@ package com.kyleposluns.ai.maze;
 import com.kyleposluns.ai.maze.solver.MazeSolver;
 import com.kyleposluns.ai.util.Location;
 
-public interface Maze<T extends MazeCell> extends MazeSolver<T> {
+public interface Maze<T extends MazeCell> {
 
 	Location getStart();
 
 	Location getFinish();
 
 	CellAccessor<T> getAccessor();
+
+	MazeSolver<T> getSolver();
 
 }
--- src/main/java/com/kyleposluns/ai/maze/solver/Node.java
+++ src/main/java/com/kyleposluns/ai/maze/solver/Node.java
@@ -1,11 +1,12 @@
 package com.kyleposluns.ai.maze.solver;
 
-import com.kyleposluns.ai.maze.MazeCell;
 import java.util.Map;
 
-public interface Node {
+public interface Node<T> {
 
-	Map<? extends MazeCell, Double> getLinks();
-	
+	Map<T, Double> getLinks();
+
+	T getWrappedObject();
+
 
 }
