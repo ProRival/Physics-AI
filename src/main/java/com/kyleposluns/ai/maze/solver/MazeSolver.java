617693be896a9290f6a56275fb44a55eaa055b4c
Kyle <igrandslam1@yahoo.com>
2017-04-22 17:08:44 -0500

    Simplified the MazeSolver interface.

--- src/main/java/com/kyleposluns/ai/maze/solver/MazeSolver.java
+++ src/main/java/com/kyleposluns/ai/maze/solver/MazeSolver.java
@@ -2,29 +2,11 @@ package com.kyleposluns.ai.maze.solver;
 
 
 import com.kyleposluns.ai.maze.MazeCell;
-
-import java.util.ArrayList;
-import java.util.Collections;
+import com.kyleposluns.ai.util.Location;
 import java.util.List;
-import java.util.Map;
 
 public interface MazeSolver<T extends MazeCell> {
 
-	List<T> solve(T origin, T destination);
-
-	default List<T> path(Map<T, T> path, T destination) {
-		assert path != null;
-		assert destination != null;
-
-		final List<T> pathList = new ArrayList<T>();
-		pathList.add(destination);
-		while (path.containsKey(destination)) {
-			destination = path.get(destination);
-			pathList.add(destination);
-		}
-		Collections.reverse(pathList);
-		return pathList;
-	}
-
+	List<T> solve(Location origin, Location destination);
 
 }
